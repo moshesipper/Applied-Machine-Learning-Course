64f412344531469f24e4912cde32b6a031f225f6
Moshe Sipper <sipper@gmail.com>
2023-01-26 10:08:44 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Applied-Machine-Learning-Course',
+    version='1.0.0',
+    url='https://github.com/moshesipper/Applied-Machine-Learning-Course',
+    author='Moshe Sipper',
+    author_email='sipper@gmail.com',
+    description='This course covers the applied side of algorithmics in machine learning and deep learning, focusing on hands-on coding experience in Python.',
+    packages=find_packages(),    
+)
